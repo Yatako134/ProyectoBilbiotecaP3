b50d6aa3367867740936dfaf23ec91e61c210822
Angel  Blas <a20222898@pucp.edu.pe>
2025-09-16 23:28:35 -0500

    Implementacion sancion y prestamo

--- ProyectoProg3/Biblioteca/src/main/java/biblioteca/main/Principal.java
+++ ProyectoProg3/Biblioteca/src/main/java/biblioteca/main/Principal.java
@@ -11,24 +11,26 @@ import pe.edu.pucp.utilsarmy.usuarios.model.Rol;
 import pe.edu.pucp.utilsarmy.usuarios.model.Usuario;
 public class Principal {
     public static void main(String[] args){
+        //Rol
+        Rol rol = new Rol("Alumno - Posgrado");
+        RolDAO daorol = new RolImpl();
+        int resultado = daorol.insertar(rol);
+        rol = daorol.obtenerPorId(resultado);
+        System.out.println(rol);
+        rol.setTipo("Alumno - Pregrado");
+        daorol.modificar(rol);
+        
         //Usuario
         Usuario user = new Usuario("Carlos","Ramos","Lopez","3490",20252324,
                 "carlosr@gmail.com","gsdf1","990923678",1);
         UsuarioImpl usuarioDAO = new UsuarioImpl();
-        int resultado = usuarioDAO.insertar(user);
+        resultado = usuarioDAO.insertar(user);
         user = usuarioDAO.obtenerPorId(resultado);
         System.out.println(user);
         user.setNombre("Miguel Jose");
         usuarioDAO.modificar(user);
         
-        //Rol
-        Rol rol = new Rol("Alumno - Posgrado");
-        RolDAO daorol = new RolImpl();
-        resultado = daorol.insertar(rol);
-        rol = daorol.obtenerPorId(resultado);
-        System.out.println(rol);
-        rol.setTipo("Alumno - Pregrado");
-        daorol.modificar(rol);
+        
         
     }
 }
