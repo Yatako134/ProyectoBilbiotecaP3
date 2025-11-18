52f2d342d75b3bd362c5f325cfbdc6e1ffd21bb3
Angel  Blas <a20222898@pucp.edu.pe>
2025-11-18 16:44:10 -0500

    Agregar scripts

---
+++ FrontEnd (C#)/BibliotecaWA/BibliotecaWA/Scripts/UtilsArmy/ModalNoUsuario.js
@@ -0,0 +1,14 @@
+﻿let modalUsuario = null;
+
+function mostrarModalUsuario(mensaje) {
+    document.getElementById("mensajeUsuario").innerText = mensaje;
+
+    modalUsuario = new bootstrap.Modal(document.getElementById("modalUsuario"));
+    modalUsuario.show();
+}
+
+function cerrarModalUsuario() {
+    if (modalUsuario) {
+        modalUsuario.hide();
+    }
+}
---
+++ FrontEnd (C#)/BibliotecaWA/BibliotecaWA/Scripts/UtilsArmy/modalRetraso.js
@@ -0,0 +1,29 @@
+﻿// ===== modalRetraso.js =====
+
+let modalRetrasoInstance = null;
+
+function mostrarModalRetraso(cantidad, prestamos) {
+
+    document.getElementById("lblCantidad").innerText = cantidad;
+
+    let cont = document.getElementById("listaPrestamos");
+    cont.innerHTML = "";
+
+    prestamos.forEach(p => {
+        cont.innerHTML += `
+            <li>
+                <b>ID:</b> ${p.idPrestamo} -
+                <b>Vence:</b> ${new Date(p.fecha_vencimiento).toLocaleDateString()}
+            </li>`;
+    });
+
+    const modal = new bootstrap.Modal(document.getElementById("modalRetraso"));
+    modal.show();
+    modalRetrasoInstance = modal;
+}
+
+function cerrarModalRetraso() {
+    if (modalRetrasoInstance) {
+        modalRetrasoInstance.hide();
+    }
+}
