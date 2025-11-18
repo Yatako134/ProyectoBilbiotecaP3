f352e6f9e699070477f8018cd91cd13d94f03bb3
JorgeRc004 <jorgeriveracal@hotmail.com>
2025-11-18 18:44:34 -0500

    Implementacion pantalla reportes (faltan r24 y r26)

---
+++ FrontEnd (C#)/BibliotecaWA/BibliotecaWA/Reportes.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BibliotecaWA
+{
+    public partial class Reportes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegistrar_click(object sender, EventArgs e)
+        {
+            if (chkReporteEjemplares != null && chkReporteEjemplares.Checked)
+            {
+                Response.Redirect("http://localhost:8080/BibliotecaWS/ReporteReq25");
+                return;
+            }
+
+            if (chkReporteUsuarios != null && chkReporteUsuarios.Checked)
+            {
+                //Response.Redirect("ReporteUsuarios.aspx");
+                return;
+            }
+
+            if (chkReporteLibros != null && chkReporteLibros.Checked)
+            {
+                //Response.Redirect("ReporteLibros.aspx");
+                return;
+            }
+
+            // Agrega aquí más checkboxes si tienes más
+            // if (chkOtro.Checked) { Response.Redirect("OtroReporte.aspx"); return; }
+
+            // Si no se marcó nada
+            ScriptManager.RegisterStartupScript(
+                this,
+                GetType(),
+                "alerta",
+                "alert('Por favor seleccione un tipo de reporte antes de descargar.');",
+                true
+            );
+
+        }
+    }
+}
