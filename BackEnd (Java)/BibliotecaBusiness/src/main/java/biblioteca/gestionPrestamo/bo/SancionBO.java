200f9e26a7d64f77070167c7034d983c0af3f370
Angel  Blas <a20222898@pucp.edu.pe>
2025-11-23 21:12:42 -0500

    Reporte

--- BackEnd (Java)/BibliotecaBusiness/src/main/java/biblioteca/gestionPrestamo/bo/SancionBO.java
+++ BackEnd (Java)/BibliotecaBusiness/src/main/java/biblioteca/gestionPrestamo/bo/SancionBO.java
@@ -3,10 +3,13 @@ package biblioteca.gestionPrestamo.bo;
 
 import biblioteca.ibo.IBO;
 import java.util.ArrayList;
+import java.util.Date;
 import pe.edu.pucp.utilsarmy.gestion_de_prestamos.model.Sancion;
 
 public interface SancionBO extends IBO<Sancion>{
     ArrayList<Sancion> listarPorUsuario(int idUsuario);
     ArrayList<Sancion> listar_busqueda_usuario(int codigo_universitario);
     ArrayList<Sancion> listarPorUsuario_X_ID(int idUsuario,String filtro);
+    int contarSancionesPorFechas(Date fechaInicio, Date fechaFin);
+
 }
